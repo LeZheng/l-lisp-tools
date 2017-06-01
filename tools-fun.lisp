76448057bcb7c1a27639127753f8722b3a638acd
skyline <skyline@localhost.localdomain>
2017-06-01 21:34:08 +0800

    add fun constructor

--- tools-fun.lisp
+++ tools-fun.lisp
@@ -203,3 +203,88 @@
     (apply fn args)
     (apply #'mapcar #'(lambda (&rest args) (apply #'rmapcar fn args)) 
            args)))
+           
+;;;------ fun constructor ------
+
+(defvar *!equivs* (make-hash-table))
+
+(defun !! (fn)
+  (or (gethash fn *!equivs*) fn))
+
+(defun def! (fn fn!)
+  (setf (gethash fn *!equivs*) fn!))
+
+(defun memoize (fn)
+  (let ((cache (make-hash-table :test #'equal)))
+        #'(lambda (&rest args)
+            (multiple-value-bind (val win) (gethash args cache)
+              (if win
+                val
+                (setf (gethash args cache) (apply fn args)))))))
+
+(defun compose (&rest fns)
+  (if fns
+    (let ((fun1 (car (last fns)))
+          (funs (butlast fns)))
+      #'(lambda (&rest args)
+          (reduce #'funcall funs
+                  :from-end t
+                  :initial-value (apply fun1 args))))
+    #'identity))
+
+(defun fif (if then &optional else)
+  #'(lambda (x)
+      (if (funcall if x)
+        (funcall then x)
+        (if else (funcall else x)))))
+
+;;function intersection (and)
+(defun fint (fn &rest fns)
+  (if (null fns)
+    fn
+    (let ((chain (apply #'fint fns)))
+      #'(lambda (x) 
+          (and (funcall fn x) (funcall chain x))))))
+
+;;function unintersrction (or)
+(defun fun (fn &rest fns)
+  (if (null fns)
+    fn
+    (let ((chain (apply #'fint fns)))
+      #'(lambda (x) 
+          (or (funcall fn x) (funcall chain x))))))
+
+;;list recurse
+(defun lrec (rec &optional base)
+  (labels ((self (lst)
+                 (if (null lst)
+                   (if (functionp base)
+                     (funcall base)
+                     base)
+                   (funcall rec (cdr lst)
+                            #'(lambda ()
+                                (self (cdr lst)))))))
+    #'self))
+
+;;;tree traverser
+(defun ttrav (rec &optional (base #'identity))
+  (labels ((self (tree)
+                 (if (atom tree)
+                   (if (functionp base)
+                     (funcall base tree)
+                     base)
+                   (funcall rec (self (car tree))
+                            (if (cdr tree)
+                              (self (cdr tree)))))))
+    #'self))
+
+(defun trec (rec &optional (base #'identity))
+  (labels ((self (tree)
+                 (if (atom tree)
+                   (if (functionp base)
+                     (funcall base tree)
+                     base)
+                   (funcall rec tree
+                            #'(lambda () (self (car tree)))
+                            #'(lambda () (if (cdr tree) (self (cdr tree))))))))
+    #'self))
