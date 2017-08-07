eef8a1dace6f6285a1c916f03d1ff2b6abada50b
LeZheng <460163461@qq.com>
2017-08-07 18:02:50 +0800

    add bind-when

--- tools-macro.lisp
+++ tools-macro.lisp
@@ -3,7 +3,9 @@
   (:nicknames "l-macro" :l-macro l-macro)
   (:export :nil!
            :while
-           :for))
+           :for
+           :bind-when
+           :bind-when*))
 
 (in-package :com.skyline.owl.lmacro)
 
@@ -19,3 +21,15 @@
   `(do ((,var ,start (funcall ,step ,var)))
      (,stop)
      ,@body))
+
+(defmacro bind-when ((var expr) &body body)
+  `(let ((,var ,expr))
+     (when ,var
+       ,@body)))
+
+(defmacro bind-when* (binds &body body)
+  (if (null binds)
+    `(prog ,@body)
+    `(let (,(car binds))
+       (if ,(caar binds)
+         (bind-when* (cdr binds) ,@body)))))
