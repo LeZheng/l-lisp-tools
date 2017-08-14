3051c67ed2f54ba43a00ff4b7bd93b4a09e2503c
LeZheng <460163461@qq.com>
2017-08-14 10:47:44 +0800

    add mvdo*

--- tools-macro.lisp
+++ tools-macro.lisp
@@ -139,3 +139,34 @@
                               `(nth ,(1- x) ,rest))))
                       len))
           (- len 2)))
+
+(defmacro mvdo* (param-cl test-cl &body body)
+  (mvdo-gen param-cl param-cl test-cl body))
+
+(defun mvdo-gen (binds rebinds test body)
+  (if (null binds)
+    (let ((label (gensym)))
+      `(prog nil
+             ,label
+             (if ,(car test)
+               (return (progn ,@(cdr test))))
+             ,@body
+             ,@(mvdo-rebind-gen rebinds)
+             (go ,label)))
+    (let ((rec (mvdo-gen (cdr binds) rebinds test body)))
+      (let ((var/s (caar binds)) (expr (cadar binds)))
+        (if (atom var/s)
+          `(let ((,var/s ,expr)) ,rec)
+          `(multiple-value-bind ,var/s ,expr ,rec))))))
+
+(defun mvdo-rebind-gen (rebinds)
+  (cond ((null rebinds) nil)
+        ((< (length (car rebinds)) 3)
+         (mvdo-rebind-gen (cdr rebinds)))
+        (t
+          (cons (list (if (atom (caar rebinds))
+                        'setq
+                        'multiple-value-setq)
+                      (caar rebinds)
+                      (third (car rebinds)))
+                (mvdo-rebind-gen (cdr rebinds))))))
